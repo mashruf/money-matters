d4570d65399b4b1fb91848546707d55a92d9b51b
mashruf <mashrufrohon@gmail.com>
2022-09-05 00:11:01 +0600

    function for getting input value added

---
+++ js/app2.js
@@ -0,0 +1,8 @@
+//function to get value from all the existing fields
+function getInput(inputField) {
+
+    const inputText = document.getElementById(inputField).value;
+    const input = parseFloat(inputText);
+    return input;
+}
+
