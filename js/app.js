c5c1c550cbdc87809dde4634eaadd09adcccb310
mashruf <mashrufrohon@gmail.com>
2022-09-05 02:26:09 +0600

    js code has been reduced and fully fixed

--- js/app.js
+++ js/app.js
@@ -1,85 +1,70 @@
-function calculateBalance() {
-    //getting value from income text input
-    const incomeInput = document.getElementById('input-income');
-    const incomeInputText = incomeInput.value;
-    const income = parseFloat(incomeInputText);
+//function to get value from all the existing fields
+function getInput(inputField) {
 
-    //getting value from food field text input
-    const foodInput = document.getElementById('input-food');
-    const foodInputText = foodInput.value;
-    const food = parseFloat(foodInputText);
-
-    //getting value from rent field text input
-    const rentInput = document.getElementById('input-rent');
-    const rentInputText = rentInput.value;
-    const rent = parseFloat(rentInputText);
-
-    //getting value from clothes field text input
-    const clothesInput = document.getElementById('input-clothes');
-    const clothesInputText = clothesInput.value;
-    const clothes = parseFloat(clothesInputText);
-
-    //getting value from total expences text area and balance text area
-    const currentTotalExpenses = document.getElementById('total-expenses');
-    const currentBalance = document.getElementById('balance');
-
-    //calculating total expenses and balance
-    const totalExpenses = food + rent + clothes;
-    const balance = income - totalExpenses;
-
-    if (income > 0) {
-        if ((food >= 0 && food <= income) && (rent >= 0 && rent <= income) && (clothes >= 0 && clothes <= income) && (totalExpenses <= income)) {
-            currentTotalExpenses.innerText = totalExpenses;
-            currentBalance.innerText = balance;
-            return balance;
-
-        }
-        else {
-            alert('Food/Rent/Clothes should not be empty and the value should not be a negative number or greater than the income. Expenses should not exceed income!');
-        }
-    }
-    else {
-        alert('Income should be more than 0')
-    }
+    const inputText = document.getElementById(inputField).value;
+    const input = parseFloat(inputText);
+    return input;
 }
 
-function calculateSavingAmountRemainingBalance() {
-    //getting value from save text input
-    const saveInput = document.getElementById('input-save');
-    const saveInputText = saveInput.value;
-    const save = parseFloat(saveInputText);
+//function to calculate total expense
+function totalExpenses() {
+    const food = getInput('input-food');
+    const rent = getInput('input-rent');
+    const clothes = getInput('input-clothes');
 
-    //calculating saving amount
-    const balance = calculateBalance();
-    if (balance >= 0) {
-        if (save >= 0 && save <= 100) {
-            //calculting saving amount
-            savingAmount = (balance * (save / 100)).toFixed(2);
-            //calculating remaining balance
-            const remainingBalance = (balance - savingAmount).toFixed(2);
-            //getting and value from total saving amount area and remaining text area
-            document.getElementById('saving-amount').innerText = savingAmount;
-            document.getElementById('remaining-balance').innerText = remainingBalance;
-        }
-        else {
-            alert('You can not save more than 100% !')
-        }
-    }
-    else {
-        alert('Please enter valid input in all fields!');
-    }
+    const totalExpenses = (food + rent + clothes).toFixed(2);
+    return totalExpenses;
+}
 
+//function to calculate balance
+function balance() {
+    const income = getInput('input-income');
+    const current_balance = (income - totalExpenses()).toFixed(2);
+    return current_balance;
+}
 
+//calculate saving amount
+function savingAmount() {
+    const save = (balance() * (getInput('input-save') / 100)).toFixed(2);
+    return save;
 
 }
 
+//calculate remaining balance
+function remainingBalance() {
+    const remainingAmount = (balance() - savingAmount()).toFixed(2);
+    return remainingAmount;
+}
 
-//calculate button event handler
+//calculate button handler
 document.getElementById('calculate').addEventListener('click', function () {
-    calculateBalance();
+    if (getInput('input-income') > 0) {
+        if (getInput('input-food') > - 0 && getInput('input-rent') >= 0 && getInput('input-clothes') >= 0) {
+            if (totalExpenses() <= getInput('input-income')) {
+                document.getElementById('total-expenses').innerText = totalExpenses();
+                document.getElementById('balance').innerText = balance();
+            }
+            else {
+                alert("Total expenses has exceeded your income!");
+            }
+        }
+
+        else {
+            alert("Negative amount or empty field or Invalid Input has been entered!");
+        }
+    }
+    else {
+        alert("Income can't be empty and should be a number greater than 0");
+    }
 });
 
-// save button event handler
+//save button handler
 document.getElementById('save').addEventListener('click', function () {
-    calculateSavingAmountRemainingBalance();
+    if (getInput('input-save') <= 100 && getInput('input-save') >= 0) {
+        document.getElementById('saving-amount').innerText = savingAmount();
+        document.getElementById('remaining-balance').innerText = remainingBalance();
+    }
+    else {
+        alert("You can't save more than more than 100% or less than 0% ");
+    }
 });
